cd68a34f1baa653f49d9eb58f033a252592b415a
svcgv <hb1230123@hotmail.com>
2018-09-28 19:45:44 +0800

    初始框架

---
+++ packageConf/dev.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var webpack = require('webpack');
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+// var htmlWebpackPlugin =  require('html-webpack-plugin');
+
+module.exports = {
+    entry: [
+      'webpack/hot/only-dev-server',
+      "./src/index.js"
+    ],
+    output: {
+        path: BUILD_PATH,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: "babel-loader",
+              query:
+                {
+                  presets:['react','es2015']
+                }
+            }, {
+                test: /\.scss$/,
+                loaders: ['style', 'css', 'sass'],
+                include: APP_PATH
+            }, {
+                test: /\.(png|jpg)$/,
+                loader: 'url?limit=40000'
+            },
+            {
+                test: /\.js$/, loader:'react-hot-loader/webpack',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve:{
+        extensions:['','.js','.json']
+    },
+    devServer: {
+        hot: true,
+        inline: true
+    },
+    plugins: [
+      new webpack.NoErrorsPlugin(),
+      new webpack.HotModuleReplacementPlugin()
+    ]
+};
