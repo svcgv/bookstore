d1b68c6af9511639b5bcb88f40c99d10f3c9ec25
svcgv <hb1230123@hotmail.com>
2018-10-02 20:12:39 +0800

    修改打包配置

--- src/Utils/connectUtil.js
+++ src/Utils/connectUtil.js
@@ -1,40 +1,52 @@
 
 
-/**
- * $1:地址，$2:参数，$3:成功回调，$4:失败回调，$5:请求方式（默认post），方式$6:超时时间
- */
-connect = (url,params,successCallBack,failCallBack,type,timeOut)=>{
-    const header = getHeader()
-    const success = successCallBack?successCallBack:defaultSuccessCall
-    const fail = failCallBack?failCallBack:defaultFailCall
-    const param = params?params:{}
-    const reqType = type?type:'POST'
-    const outTime = timeOut?timeOut:3000
+class connect{
+    /**
+     * $1:地址，$2:参数，$3:成功回调，$4:失败回调，$5:请求方式（默认post），方式$6:超时时间
+     */
+    jsonConnect = (url,params,successCallBack,failCallBack,type,timeOut)=>{
+        const header = getHeader()
+        const success = successCallBack?successCallBack:defaultSuccessCall
+        const fail = failCallBack?failCallBack:defaultFailCall
+        const options = params?params:{}
+        const reqType = type?type:'POST'
+        const outTime = timeOut?timeOut:3000
 
-    var a = fetch()
-}
+        var myInit = { method: reqType,
+                headers: header,
+                body: JSON.stringify(options),
+                mode: 'cors',
+                cache: 'default' };
+        fetch(url, myInit).then(success(response), fail(error) )
+    }
 
-/**
- * 设置跨域请求头
- */
-getHeader = ()=>{
-    let header = {};
+    download = (url,params,successCallBack,failCallBack,type,rspFileType)=>{
 
-    return header;
-}
+    }
+    /**
+     * 设置跨域请求头
+     */
+    getHeader = ()=>{
+        let myHeaders = new Headers();
+        //Todo 设置跨域请求头
+        myHeaders.append('Content-Type', 'text/xml');
+        myHeaders.get('Content-Type');
+        return header;
+    }
 
-/**
- * 默认成功回调
- */
-defaultSuccessCall = (res)=>{
-    console.log(res)
-}
+    /**
+     * 默认成功回调
+     */
+    defaultSuccessCall = (res)=>{
+        console.log(res)
+    }
 
-/**
- * 默认失败回调
- */
-defaultFailCall = (err)=>{
-    console.log(err)
+    /**
+     * 默认失败回调
+     */
+    defaultFailCall = (err)=>{
+        console.log(err)
+    }
 }
 
 export default new connect
